c0dd5308f8708f96bc700f2501d38ea300c7164e
Raheel Khan <irahilkhan@gmail.com>
2020-07-02 23:15:54 +0500

    JS objects

--- Basic Javascript/Manipulate Arrays With unshift().js
+++ Basic Javascript/Manipulate Arrays With unshift().js
@@ -4,3 +4,8 @@ myArray.shift();
 
 // Only change code below this line
 myArray.unshift(["Paul", 35])
+
+var myProgress = ['Done with JS', 40];
+console.log(myProgress[0] + " : " + myProgress[1]);
+myProgress.push(["Moving Forward to", "Lists"]);
+console.log(myProgress[2][0] + " : " + myProgress[2][1]);
